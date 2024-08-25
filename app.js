2430a09a1a4ffc586fa0b6aa6a2df2d36c3abbf6
githubscientist <sathish.datascientist@gmail.com>
2024-08-25 12:14:45 +0530

    starting the express app

---
+++ app.js
@@ -0,0 +1,8 @@
+// import the express module
+const express = require('express');
+
+// create a new express app
+const app = express();
+
+// export the app
+module.exports = app;
--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 // connect to the database
 const mongoose = require('mongoose');
 const { MONGODB_URI } = require('./utils/config');
+const app = require('./app');
 
 mongoose.connect(MONGODB_URI)
-    .then(() => console.log('Connected to MongoDB...'))
+    .then(() => {
+        console.log('Connected to MongoDB...');
+
+        // start the server and listen on port 3001
+        app.listen(3001, () => {
+            console.log('Server is running on port 3001 at http://localhost:3001');
+        });
+    })
     .catch(err => console.error('Could not connect to MongoDB...', err));
